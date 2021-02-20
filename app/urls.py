d6b0e9aea2c423529139bdcf0b031f2867a47357
botty00 <hika.8351@gmail.com>
2021-02-20 15:29:17 +0900

    commit3

---
+++ app/urls.py
@@ -0,0 +1,20 @@
+from django.contrib import admin
+from django.urls import path,include
+from . import views
+
+urlpatterns = [
+    path("hello",views.hello,name="hello"),
+    path('create_user',views.create_user,name='create_user'),
+    path('loginview',views.loginview,name='loginview'),
+    path('question1',views.question1,name='question1'),
+    path('question2',views.question2,name='question2'),
+    path('question3',views.question3,name='question3'),
+    path('answer/<int:correct>',views.answer,name='answer'),
+    path('answer2/<int:correct>',views.answer2,name='answer2'),
+    path('answer3/<int:correct>',views.answer3,name='answer3'),
+    path('result',views.result,name='result'),
+    path('start',views.start,name='start'),
+    path('top',views.top,name='top'),
+    path('complete',views.complete,name='complete'),
+    path('logoutview',views.logoutview,name='logoutview'),
+]

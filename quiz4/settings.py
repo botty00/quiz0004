86586bd6502d1623266209b762fdd21fb23b47b1
botty00 <hika.8351@gmail.com>
2021-02-27 19:31:00 +0900

    commit04

--- quiz4/settings.py
+++ quiz4/settings.py
@@ -83,7 +83,7 @@ WSGI_APPLICATION = 'quiz4.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-'''
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
@@ -105,7 +105,7 @@ DATABASES = {
         'PORT': '5432',
 
 }
-
+'''
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
